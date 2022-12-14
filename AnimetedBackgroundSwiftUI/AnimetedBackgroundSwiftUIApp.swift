e925a9273345931b85e2cf0827e60090edfd9e70
Bertug <ber2horoz@gmail.com>
2022-12-14 14:25:54 +0300

    Initial Commit

---
+++ AnimetedBackgroundSwiftUI/AnimetedBackgroundSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimetedBackgroundSwiftUIApp.swift
+//  AnimetedBackgroundSwiftUI
+//
+//  Created by Bertuğ Horoz on 14.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimetedBackgroundSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
